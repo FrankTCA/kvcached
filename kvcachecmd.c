43e3b0039a1c2202257e8653259613ffb491a18d
Aaron-Speedy <aaron@lspeedy.org>
2024-10-04 13:16:45 -0500

    Better argument parsing for kvcachecmd

--- kvcachecmd.c
+++ kvcachecmd.c
@@ -2,23 +2,56 @@
 #include "ds.h"
 #include "print_functions.h"
 
-void usage_err(char *argv[]) {
-  fprintf(stderr, "Usage: %s 'S|G|D|C' <key> <val>\n", argv[0]);
+typedef struct {
+  int argc;
+  char **argv;
+  int i;
+  s8 extra_usage;
+} ArgParser;
+
+void usage_err(ArgParser a) {
+  fprintf(stderr, "Usage: %s <port> ", a.argv[0]);
+  s8_print(a.extra_usage);
+  printf("\n");
   exit(1);
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) usage_err(argv);
-  if (strlen(argv[1]) != 1) usage_err(argv);
+s8 get_next_arg(ArgParser *a) {
+  if (a->i >= a->argc) usage_err(*a);
 
-  u8 command = argv[1][0];
+  s8 ret = { .buf = a->argv[a->i], };
+  ret.len = strlen(ret.buf);
+
+  a->i += 1;
+
+  return ret;
+}
+
+void no_more_args(ArgParser a) {
+  if (a.argc > a.i) usage_err(a);
+}
+
+int main(int argc, char *argv[]) {
+  ArgParser argp = { argc, argv, 1, };
+
+  u16 port;
+  u8 command;
+  {
+    argp.extra_usage = s8("'S|G|D|C' ...");
+    s8 p = get_next_arg(&argp);
+    s8 c = get_next_arg(&argp);
+    if (c.len != 1) usage_err(argp);
+
+    port = atoi(p.buf);
+    command = c.buf[0];
+  }
 
   switch (command) {
   case 'S': {
-    if (argc < 4) usage_err(argv);
-
-    s8 key = { .buf = (u8 *) argv[2], .len = strlen(argv[2]), };
-    s8 val = { .buf = (u8 *) argv[3], .len = strlen(argv[3]), };
+    argp.extra_usage = s8("S <key> <val>");
+    s8 key = get_next_arg(&argp);
+    s8 val = get_next_arg(&argp);
+    no_more_args(argp);
 
     printf("%c", command);
     print_u32_bytes(key.len);
@@ -27,25 +60,27 @@ int main(int argc, char *argv[]) {
     s8_print(val);
   } break;
   case 'G': {
-    if (argc != 3) usage_err(argv);
-
-    s8 key = { .buf = (u8 *) argv[2], .len = strlen(argv[2]), };
+    argp.extra_usage = s8("G <key>");
+    s8 key = get_next_arg(&argp);
+    no_more_args(argp);
 
     printf("%c", command);
     print_u32_bytes(key.len);
     s8_print(key);
   } break;
   case 'D': {
-    if (argc != 3) usage_err(argv);
-
-    s8 key = { .buf = (u8 *) argv[2], .len = strlen(argv[2]), };
+    argp.extra_usage = s8("D <key>");
+    s8 key = get_next_arg(&argp);
+    no_more_args(argp);
 
     printf("%c", command);
     print_u32_bytes(key.len);
     s8_print(key);
   } break;
   case 'C': {
-    if (argc != 2) usage_err(argv);
+    argp.extra_usage = s8("C");
+    no_more_args(argp);
+
     printf("%c", command);
   } break;
   default: assert(!"Unimplemented or unrecognized command");
